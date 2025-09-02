b1a1110d9643687793e286c5d91b5768bbfbfa67
hmargolis1 <hmargolis1@hwemail.com>
2025-09-02 13:40:04 -0700

    Update Beanbag.java

--- DAY 0/Beanbag.java
+++ DAY 0/Beanbag.java
@@ -3,19 +3,21 @@ public class Beanbag
     private int wheels;
     private double legs;
     private boolean cushion;
-    private char firstLetterOfColor;
+    private char color;
     private String size;
     private String location;
+    public int poundsOfLining;
     public Beanbag()
     {
         wheels = 4;
         legs = 3.58;
         cushion = true;
-        firstLetterOfColor = 'b';
+        color = 'b';
         size = "large";  
         location = "Library";
+        poundsOfLining = 30;
      }
-     //sit, jump, push, tear
+     //sit, jump, push, tear, cutLining
      public void sit()
      {
         wheels = 3;
@@ -35,5 +37,21 @@ public class Beanbag
      {
         cushion = false;
         System.out.println("There is a cushion: " + cushion + ", after a tear.");
+     }
+     public void cutLining(int poundsToCut)
+     {
+      if(poundsOfLining < poundsToCut)
+      {
+         System.out.println("Cannot cut that much beanbag lining");
+      }
+      if(poundsToCut < 0)
+      {
+          System.out.println("Cannot cut negative pounds");
+      }
+      else {
+         poundsOfLining-= poundsToCut;
+      }
+
+       
      }
 }
