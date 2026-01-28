94350e27951331d247f356b3c0f8cdec5d9edee1
hmargolis1 <hmargolis1@hwemail.com>
2026-01-27 21:59:45 -0800

    Finished SkyView

--- SkyView Lab/SkyView.java
+++ SkyView Lab/SkyView.java
@@ -11,6 +11,10 @@ public class SkyView {
         if (numberOfRows < 0 || numberOfCols < 0) {
             throw new IllegalArgumentException("Negative Rows/Cols");
         }
+        if (scanned.length < numberOfRows * numberOfCols) {
+            throw new IllegalArgumentException("Array does not fit these dimensions, "
+                    + "even though this was a precondition...");
+        }
         view = new double[numberOfRows][numberOfCols];
         int count = 0;
         for (int r = 0; r < numberOfRows; r++) {
@@ -46,15 +50,38 @@ public class SkyView {
     }
 
     public boolean equals(SkyView other) {
-
+        if (other == null) {
+            throw new IllegalArgumentException("Object null");
+        }
+        if (view == null) {
+            throw new IllegalArgumentException("View null");
+        }
         for (int r = 0; r < view.length; r++) {
             for (int c = 0; c < view[0].length; c++) {
-                if (view[r][c] != other[r][c]) {
+                if (view[r][c] != other.view[r][c]) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public double getAverage(int startRow, int endRow, int startCol, int endCol) {
+        if (startRow < 0 || endRow < 0 || startCol < 0 || endCol < 0) {
+            throw new IllegalArgumentException("No negative row or column.");
+        }
+        if (endRow > view.length || endCol > view[0].length) {
+            throw new IllegalArgumentException("Row and Column are out of bounds.");
+        }
+        double topRow = 0.0;
+        int count = 0;
+        for (int r = startRow; r <= endRow; r++) {
+            for (int c = startCol; c <= endCol; c++) {
+                topRow += view[r][c];
+                count++;
+            }
+        }
+        return topRow / (double) count;
     }
 
     public double[][] getView() {
--- SkyView Lab/SkyViewTester.java
+++ SkyView Lab/SkyViewTester.java
@@ -49,10 +49,75 @@ public class SkyViewTester {
     }
 
     public static void testEquals() {
-
+        try {
+            double[] first = { 1.1, 2.2, 3.3, 4.4 };
+            double[] second = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView firstSky = new SkyView(2, 2, first);
+            SkyView secondSky = new SkyView(2, 2, second);
+            System.out.println(firstSky.equals(secondSky));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] first = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView firstSky = new SkyView(2, 2, first);
+            SkyView secondSky = new SkyView(2, 2, null);
+            System.out.println(firstSky.equals(secondSky));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] first = { 1.1, 2.2, 3.3, 4.4 };
+            double[] second = { 1.1, 2.2, 3.3, 1.0 };
+            SkyView firstSky = new SkyView(2, 2, first);
+            SkyView secondSky = new SkyView(2, 2, second);
+            System.out.println(firstSky.equals(secondSky));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] second = { 1.1, 2.2, 3.3, 1.0 };
+            SkyView firstSky = new SkyView(2, 2, null);
+            SkyView secondSky = new SkyView(2, 2, second);
+            System.out.println(firstSky.equals(secondSky));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] first = {};
+            double[] second = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView firstSky = new SkyView(2, 2, first);
+            SkyView secondSky = new SkyView(2, 2, second);
+            System.out.println(firstSky.equals(secondSky));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public static void testGetAverage() {
+        try {
+            double[] scanned = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView newSky = new SkyView(2, 2, scanned);
+            System.out.println(newSky.getAverage(0, 1, 0, 1));
 
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] scanned = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView newSky = new SkyView(2, 2, scanned);
+            System.out.println(newSky.getAverage(0, 0, 0, 1));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            double[] scanned = { 1.1, 2.2, 3.3, 4.4 };
+            SkyView newSky = new SkyView(2, 2, scanned);
+            System.out.println(newSky.getAverage(0, -1, 0, 1));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
